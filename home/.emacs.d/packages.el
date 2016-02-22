c51da156410fca193597f9a311bf3bb25d81285c
David Boot <kodnin@gmail.com>
2016-02-22 21:23:07 +0100

    Add Slime to Emacs

--- home/.emacs.d/packages.el
+++ home/.emacs.d/packages.el
@@ -60,6 +60,12 @@
 (use-package sass-mode
   :ensure t)
 
+(use-package slime
+  :ensure t
+  :init
+  (setq inferior-lisp-program "/usr/local/bin/sbcl")
+  (setq slime-contribs '(slime-fancy)))
+
 (use-package web-mode
   :ensure t
   :init
