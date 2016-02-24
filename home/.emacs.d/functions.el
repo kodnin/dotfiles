b15aa5ff7f026714a3f1bf3b8b9d4f7f97ae16a9
David Boot <kodnin@gmail.com>
2016-02-24 22:17:25 +0100

    Extract functions from init.el for Emacs

---
+++ home/.emacs.d/functions.el
@@ -0,0 +1,23 @@
+(defun functions-open ()
+  (interactive)
+  (find-file "~/.emacs.d/functions.el"))
+
+(defun functions-load ()
+  (interactive)
+  (load "~/.emacs.d/functions.el"))
+
+(defun init-open ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(defun init-load ()
+  (interactive)
+  (load "~/.emacs.d/init.el"))
+
+(defun packages-open ()
+  (interactive)
+  (find-file "~/.emacs.d/packages.el"))
+
+(defun packages-load ()
+  (interactive)
+  (load "~/.emacs.d/packages.el"))
--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,5 +1,6 @@
-;;;; packages
+;;;; files
 
+(load "~/.emacs.d/functions.el")
 (load "~/.emacs.d/packages.el")
 
 ;;;; ui
@@ -33,21 +34,3 @@
 (setq scroll-step 1)
 (setq-default truncate-lines 1)
 (show-paren-mode 1)
-
-;;;; functions
-
-(defun init-open ()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-
-(defun init-load ()
-  (interactive)
-  (load "~/.emacs.d/init.el"))
-
-(defun packages-open ()
-  (interactive)
-  (find-file "~/.emacs.d/packages.el"))
-
-(defun packages-load ()
-  (interactive)
-  (load "~/.emacs.d/packages.el"))
